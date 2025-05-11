f5f94e4b2370e34b0b6e9620ea69197fe9389814
Gabriel Juárez <165744178+GabJuarez@users.noreply.github.com>
2025-05-11 16:35:26 -0600

    correcciones, slicing en la impresión y método peek

--- Ejercicios_pilas_colas/pilas/ejercicio5.py
+++ Ejercicios_pilas_colas/pilas/ejercicio5.py
@@ -4,36 +4,45 @@ Al llegar a destino, el primer saco que se descarga es el último que se cargó.
 con una pila que permita apilar sacos (push), descargar uno (pop) y visualizar el que está encima (peek).
 '''
 
-class mercado():
+class Mercado:
     
     def __init__(self):
-        pass
+        self.sacos = []  
     
     def pedir_sacos(self):
-        self.sacos = []
         seq = int(input('Cuantos sacos desea agregar: '))
         for i in range(seq):
-            nombre = (input(f'Ingrese el nombre del saco {i+1}: '))
+            nombre = input(f'Ingrese el nombre del saco {i+1}: ')
             self.sacos.append(nombre)
-        print(self.sacos)
-            
-    def descargar_sacos(self):
-        print('Cuantos sacos desea descargar: ')
-        n = int(input())
-        if n > len(self.sacos):
-            print('Esta accion no se puede realizar')
-        else:
-            for i in range(n):
-                print(f'Descargando el saco numero {i+1}')
-                self.sacos.pop()
-            print('Pila restante: ')
-            print(self.sacos)
-                
-    def mostrar_pila(self):
-        for i in range (len(self.sacos)):
-            print(f'Revisando saco con nombre: {self.sacos[i]}')
+        print("\n")
+        print("Sacos apilados:", self.sacos)
     
-new_mercado = mercado()
+    def descargar_saco(self):
+        if not self.sacos:
+            print("No hay sacos para descargar, la pila está vacia")
+        else:
+            saco_descargado = self.sacos.pop()  
+            print(f"Saco descargado: {saco_descargado}")
+        print("\n")
+
+    def mostrar_pila(self):
+        if not self.sacos:
+            print("La pila está vacía")
+        else:
+            print("Sacos en la pila:")
+            for i, saco in enumerate(self.sacos[::-1], start=1):  #slicing para imprimir los datos de manera invertida
+                print(f"{i}. {saco}")
+        print("\n")
+
+    def peek(self):
+        if not self.sacos:
+            print("La pila está vacía, no hay sacos para mostrar.")
+        else:
+            print(f'El siguiente saco a descargar es: {self.sacos[-1]}')
+
+#instancia
+new_mercado = Mercado()
 new_mercado.pedir_sacos()
-new_mercado.descargar_sacos()
+new_mercado.descargar_saco()
 new_mercado.mostrar_pila()
+new_mercado.peek()
